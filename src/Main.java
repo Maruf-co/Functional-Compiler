dd6a88c868c8cb3f9cabaf5ea5886910cc2ef92a
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-02-20 18:06:55 +0300

    add: added a showcase (read the input.txt and print the parsed token names in the same order)

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,30 @@
-import tokens.Token;
 
+import tokens.*;
+import lex.Lexer;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
+
 public class Main {
-
-    public static void main(String[] args) {
-        ArrayList<Token> tokens = new ArrayList<>();
-
+    
+    public static ArrayList<Token> tokens = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        Lexer lexer = new Lexer(new FileReader("input.txt"));
+        while (!lexer.yyatEOF()) {
+            Token token = lexer.yylex();
+            if (token == null) {
+                break;
+            }
+            System.out.print(token.getName() + " ");
+        }
+        printTokens();
     }
 
-    void printTokens(ArrayList<Token> tokens) {
+    static void printTokens() {
         for (Token token: tokens){
-            System.out.print(token.getName());
+            System.out.print(token.getName() + "");
         }
     }
 }
